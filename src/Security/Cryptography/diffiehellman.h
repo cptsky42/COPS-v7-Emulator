47fa8108d745d5f9ac4841665d3256d98e1c822c
Jean-Philippe Boivin <jp.boivin@barlab.org>
2013-12-23 14:14:38 -0500

    Added documentation to DH.

--- src/Security/Cryptography/diffiehellman.h
+++ src/Security/Cryptography/diffiehellman.h
@@ -17,24 +17,56 @@ extern "C" {
 #include "bigint.h"
 }
 
+/**
+ * Diffie-Hellman key exchange implementation.
+ */
 class DiffieHellman
 {
 public:
+    /**
+     * Create a new exchange based on specified p & g keys.
+     *
+     * @param[in]   aP     the p key as a base 16 string
+     * @param[in]   aG     the g key as a base 16 string
+     */
     DiffieHellman(char* aP, char* aG);
+
+    /* destructor */
     ~DiffieHellman();
 
+    /**
+     * Generate a new request.
+     *
+     * @returns the A key as a base 16 string
+     */
     std::string generateRequest();
+
+    /**
+     * Generate a new response based on the public A key.
+     *
+     * @param[in]   aPubKey    the public request (A key)
+     *
+     * @returns the B key as a base 16 string
+     */
     std::string generateResponse(char* aPubKey);
+
+    /**
+     * Handle the response (B key) to generate the final key (s key).
+     *
+     * @param[in]   aPubKey    the public response (B key)
+     *
+     * @returns the s key as a base 16 string
+     */
     std::string handleResponse(char* aPubKey);
 
 private:
-    bigint p;
-    bigint g;
-    bigint a;
-    bigint b;
-    bigint s;
-    bigint A;
-    bigint B;
+    bigint p; //!< public p key
+    bigint g; //!< public g key
+    bigint a; //!< private a key (request)
+    bigint b; //!< private b key (response)
+    bigint s; //!< final key (s)
+    bigint A; //!< public A key (request)
+    bigint B; //!< public B key (response)
 };
 
 #endif // _COPS_V7_EMULATOR_DIFFIE_HELLMAN_H_
